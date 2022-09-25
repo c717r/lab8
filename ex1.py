7ab63e19d465bc5262939d56375fb12caed6a1de
Jamshid <jallayorov39@gmail.com>
2022-09-25 21:19:07 +0300

    done

---
+++ ex1.py
@@ -0,0 +1,10 @@
+def positive():print("«Положительное»")
+def negative():print("«Отрицательное»")
+
+number = int(input("Enter an enteger : "))
+
+def test(): number = int(input("Enter an enteger : ")) 
+if number > 0: positive()
+else: negative()
+
+
---
+++ ex3.py
@@ -0,0 +1,8 @@
+from math import prod
+ 
+ 
+def foo():
+    return prod(iter(lambda: int(input()), 0))
+    
+result = foo()
+print(result)
